b870eac3f361c599a15f563cab697be1649d7322
Kane York <rikingcoding@gmail.com>
2016-01-17 19:51:10 -0800

    okay yeah i'm tired of that buildtime penalty

--- socketserver/server/tickspersecond.go
+++ socketserver/server/tickspersecond.go
@@ -4,9 +4,9 @@ package server
 // long get_ticks_per_second() {
 //   return sysconf(_SC_CLK_TCK);
 // }
-import "C"
+//import "C"
 
 // note: this seems to add 0.1s to compile time on my machine
-var ticksPerSecond = int(C.get_ticks_per_second())
+//var ticksPerSecond = int(C.get_ticks_per_second())
 
-//var ticksPerSecond = 100
+var ticksPerSecond = 100
