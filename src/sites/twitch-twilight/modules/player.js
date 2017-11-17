a45dc472b78031c9a1b3f8b76d34f202e0570824
SirStendec <sir@stendec.me>
2017-11-17 14:56:18 -0500

    Fix volume scroll with Firefox. Closes #310

--- src/sites/twitch-twilight/modules/player.js
+++ src/sites/twitch-twilight/modules/player.js
@@ -163,7 +163,7 @@ export default class Player extends Module {
 
 		} else if ( enabled && ! inst._ffz_scroll_handler ) {
 			inst.playerRef.addEventListener('wheel', inst._ffz_scroll_handler = e => {
-				const delta = e.wheelDelta || -e.detail,
+				const delta = e.wheelDelta || e.deltaY || -e.detail,
 					player = inst.player;
 
 				if ( player ) {
