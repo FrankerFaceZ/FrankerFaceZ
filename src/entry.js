691c09eb18d66d4393d23967690cdc67ce11bbdd
SirStendec <sir@stendec.me>
2017-12-01 15:31:59 -0500

    Don't try running FFZ on the blog sub-domain.

--- src/entry.js
+++ src/entry.js
@@ -2,7 +2,7 @@
 'use strict';
 (() => {
 	// Don't run on certain sub-domains.
-	if ( /^(?:player|im|chatdepot|tmi|api|)\./.test(location.hostname) )
+	if ( /^(?:blog|player|im|chatdepot|tmi|api|)\./.test(location.hostname) )
 		return;
 
 	const DEBUG = localStorage.ffzDebugMode == 'true' && document.body.classList.contains('ffz-dev') && ! window.Ember,
