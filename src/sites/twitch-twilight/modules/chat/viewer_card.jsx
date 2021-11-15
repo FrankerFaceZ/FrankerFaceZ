cedf5503bb884ab7c7681156ad6128e9bdf0af93
Jamah Akbary <jamah.akbary@mni.thm.de>
2021-11-15 11:04:17 +0100

    hiding sub gift button on viewerCard

--- src/sites/twitch-twilight/modules/chat/viewer_card.jsx
+++ src/sites/twitch-twilight/modules/chat/viewer_card.jsx
@@ -17,6 +17,16 @@ export default class ViewerCards extends Module {
 
 		this.last_login = null;
 
+		this.settings.add('chat.viewer-cards.hide-gift-subscribe-button', {
+			default: '',
+			ui: {
+				path: 'Chat > Viewer Cards >> Appearance',
+				title: 'Hide the gift-subscribe-button on the viewer card',
+				component: 'setting-check-box'
+			},
+			changed: val => this.css_tweaks.toggle('hide-gift-subscribe-button', val)
+		});
+
 		this.settings.add('chat.viewer-cards.highlight-chat', {
 			default: false,
 			ui: {
@@ -52,6 +62,7 @@ export default class ViewerCards extends Module {
 	onEnable() {
 		this.chat.context.on('changed:chat.viewer-cards.highlight-chat', this.refreshStyle, this);
 		this.chat.context.on('changed:chat.viewer-cards.color', this.refreshStyle, this);
+		this.toggle('hide-gift-subscribe-button', this.settings.get('chat.viewer-cards.hide-gift-subscribe-button'));
 		this.on('..:update-colors', this.refreshStyle, this);
 
 		this.ViewerCard.on('mount', this.updateCard, this);
