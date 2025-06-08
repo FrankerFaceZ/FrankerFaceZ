5c11a0fcba9c214a4af3b76df280a3a9ed5a2d7c
lemonslut <lemonslut11@gmail.com>
2025-06-07 22:19:30 -0600

    eslint: comment out empty onEnable method

--- src/socket.js
+++ src/socket.js
@@ -125,14 +125,14 @@ export default class SocketClient extends Module {
 	}
 
 
-	onEnable() {
-		// We don't connect anymore.
-		// For now, stop connecting to the sockets for people using the
-		// API links experiment.
-		//if ( this.experiments.getAssignment('api_links') )
-		//	return;
-		//this.connect();
-	}
+	// onEnable() {
+	// 	We don't connect anymore.
+	// 	For now, stop connecting to the sockets for people using the
+	// 	API links experiment.
+	// 	if ( this.experiments.getAssignment('api_links') )
+	// 	return;
+	// 	this.connect();
+	// }
 	onDisable() { this.disconnect() }
 
 
